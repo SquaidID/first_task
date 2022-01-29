21507ae31026c6b12fedd2450f9460d0b079f74a
Vladimir <boban174@gmail.com>
2022-01-30 02:36:10 +0500

    complete first task

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-"use strict"
+"use strict";
+
+const numberOfFilms = +prompt("how many films have you watched");
+//console.log(typeof(numberOfFilms));
+
+const personalMoviDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false,
+};
+
+const a = prompt("Last watched movie", '');
+const b = prompt("Rate?", '');
+const c = prompt("Last watched movie", '');
+const d = prompt("Rate?", '');
+
+personalMoviDB.movies[a] = b;
+personalMoviDB.movies[c] = d;
