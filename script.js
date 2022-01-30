178714cf0c09c7f05a559b64e9f5904040ae5d60
Vladimir <boban174@gmail.com>
2022-01-31 01:34:37 +0500

    task_2_complite

--- script.js
+++ script.js
@@ -11,10 +11,26 @@ const personalMoviDB = {
     private: false,
 };
 
-const a = prompt("Last watched movie", '');
-const b = prompt("Rate?", '');
-const c = prompt("Last watched movie", '');
-const d = prompt("Rate?", '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Last watched movie", '');
+    const b = prompt("Rate?", '');
+    if (a != null && b != null && a != '' && b != '' && a.length<50){
+        personalMoviDB.movies[a] = b;
+    }else{
+        i--;
+    }
+    
+    console.log(a);
+    console.log(b);
+}
+console.log(personalMoviDB);
 
-personalMoviDB.movies[a] = b;
-personalMoviDB.movies[c] = d;
+if (personalMoviDB.count < 10){
+    alert("МАЛА");
+} else if(personalMoviDB.count < 30){
+    alert("НОРМ");
+}else if(personalMoviDB.count >= 30){
+    alert("МНОГА");
+}else{
+    alert("ERROR");
+}
